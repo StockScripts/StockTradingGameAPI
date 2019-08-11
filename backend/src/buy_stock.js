b042767ddcd8739128486e60022fd6609713f42d
Brighton Balfrey <balfrey@usc.edu>
2019-08-11 12:25:10 -0700

    Added rp call to IEX api

--- backend/src/buy_stock.js
+++ backend/src/buy_stock.js
@@ -1,4 +1,13 @@
+var rp = require('request-promise');
 
+const get_stock_price = (ticker) => {
+    var options = {
+        //method: 'GET',
+        uri: 'https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_6b9e8401a74644dbb8cb97ec42bcaeee',
+        json: true
+    }
+    return rp(options);
+};
 
 exports.handler = (event, context, callback) => {
 	const done = (err, res) => {
@@ -11,5 +20,14 @@ exports.handler = (event, context, callback) => {
         };
         err ? callback(res) : callback(null, res);
     }
-	done(null, {hi: 'hi'});
+    
+    get_stock_price('aapl')
+    .then(data => {
+        console.log(data);
+        done(null, data);
+    })
+    .catch(err => {
+        done(err);
+    });
 }
+//curl -k 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_6b9e8401a74644dbb8cb97ec42bcaeee'
--- backend/src/sell_stock.js
+++ backend/src/sell_stock.js
@@ -1,3 +1,12 @@
+const get_stock_price = (ticker) => {
+    var options = {
+        //method: 'GET',
+        uri: 'https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_6b9e8401a74644dbb8cb97ec42bcaeee',
+        json: true
+    }
+    return rp(options);
+};
+
 exports.handler = (event, context, callback) => {
 	const done = (err, res) => {
 		let response = {
